173ba3eb98b3f8946f9ea0f47450a9a303f2e6ca
garoloup <fredwolff@free.fr>
2020-04-05 19:06:49 +0200

    First version to send URL to download station on right click

---
+++ SendLink.js
@@ -0,0 +1,208 @@
+/* +++++++++++++++++++++++++++++++++
+Create context menu item
+*/
+//browser.contextMenus.create({
+chrome.contextMenus.create({
+    id: "copy-link-to-clipboard",
+    title: chrome.i18n.getMessage("menuContextSendLink"),
+contexts: ["link"],
+},onCreated);
+
+/* +++++++++++++++++++++++++++++++++
+Listen to new context menu item
+and start QNAP steps for download
+*/
+//browser.contextMenus.onClicked.addListener((info, tab) => {
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+    if (info.menuItemId === "copy-link-to-clipboard") {
+        // Examples: text and HTML to be copied.
+        const text = "This is text: " + info.linkUrl;
+        // Always HTML-escape external input to avoid XSS.
+        const safeUrl = escapeHTML(info.linkUrl);
+        const html = `This is HTML: <a href="${safeUrl}">${safeUrl}</a>`;
+
+        console.log("Send to QNAP URL="+safeUrl);
+        LoadAndLogAndAddUrl(safeUrl);
+    }
+});
+
+// https://gist.github.com/Rob--W/ec23b9d6db9e56b7e4563f1544e0d546
+function escapeHTML(str) {
+    // Note: string cast using String; may throw if `str` is non-serializable, e.g. a Symbol.
+    // Most often this is not the case though.
+    return String(str)
+        .replace(/&/g, "&amp;")
+        .replace(/"/g, "&quot;").replace(/'/g, "&#39;")
+        .replace(/</g, "&lt;").replace(/>/g, "&gt;");
+}
+
+
+
+
+/*
+Called when the item has been created, or when creation failed due to an error.
+We'll just log success/failure here.
+*/
+function onCreated() {
+//  if (browser.runtime.lastError) {
+  if (chrome.runtime.lastError) {
+    console.log(`Error: ${chrome.runtime.lastError}`);
+  } else {
+    console.log("Item created successfully");
+  }
+}
+
+/*
+Called when the item has been removed.
+We'll just log success here.
+*/
+function onRemoved() {
+  console.log("Item removed successfully");
+}
+
+/*
+Called when there was an error.
+We'll just log the error here.
+*/
+function onError(error) {
+  console.log(`Error: ${error}`);
+}
+
+
+/* +++++++++++++++++++++++++++++++++
+QNAP settings vars
+*/
+var NASaddr = "0.0.0.0";
+var NASport = "80";
+var NASlogin = "";
+var NASpassword = "";
+var NASdir = "";
+
+function onError(error) {
+    console.log(`Error: ${error}`);
+  }
+
+/* +++++++++++++++++++++++++++++++++
+Load stored QNAP settings to local vars
+*/
+/*function initialize() {
+//  var gettingAllStorageItems = browser.storage.local.get(null);
+  var gettingAllStorageItems = chrome.storage.local.get(null);
+  gettingAllStorageItems.then((res) => {
+      NASaddr = res.NASaddress;
+      NASport = res.NASport;
+      NASlogin = res.NASlogin;
+      NASpassword = res.NASpassword;
+      NASdir = res.NASdir;
+  }, onError);
+}*/
+
+function initialize() {
+//  var gettingAllStorageItems = browser.storage.local.get(null);
+  chrome.storage.local.get(null,function(res) {
+      NASaddr = res.NASaddress;
+      NASport = res.NASport;
+      NASlogin = res.NASlogin;
+      NASpassword = res.NASpassword;
+      NASdir = res.NASdir;
+  });
+}
+//browser.storage.onChanged.addListener(initialize);
+chrome.storage.onChanged.addListener(initialize);
+
+/* +++++++++++++++++++++++++++++++++
+Section of HTTP request to send download URL to QNAP
+ +++++++++++++++++++++++++++++++++ */
+
+function handleCreatedbyXHR(item) {
+
+   LoadAndLogAndAddUrl(item.url) ;
+}
+
+/* +++++++++++++++++++++++++++++++++
+Load NAS settings and call next steps
+*/
+function LoadAndLogAndAddUrl(url) {
+//  var gettingAllStorageItems = browser.storage.local.get(null);
+/*  var gettingAllStorageItems = chrome.storage.local.get(null);
+  gettingAllStorageItems.then((res) => {
+      NASaddr = res.NASaddress;
+      NASport = res.NASport;
+      NASlogin = res.NASlogin;
+      NASpassword = res.NASpassword;
+      NASdir = res.NASdir;*/
+
+    chrome.storage.local.get(null,function(res) {
+      NASaddr = res.NASaddress;
+      NASport = res.NASport;
+      NASlogin = res.NASlogin;
+      NASpassword = res.NASpassword;
+      NASdir = res.NASdir;
+    console.log("settings:"+res.NASlogin+":"+res.NASpassword+"@"+res.NASaddress+":"+res.NASport+"/".NASdir);
+     LogAndAddUrl(url);
+  });
+}
+
+/* +++++++++++++++++++++++++++++++++
+ Login into NAS and get SID for next step
+*/
+function LogAndAddUrl(url) {
+    var data = "";
+    var xhr = new XMLHttpRequest();
+
+    data = "user="+NASlogin+"&pass="+btoa(NASpassword);
+    console.log("param login ="+data);
+    xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function() {
+  if(this.readyState === 4) {
+    console.log(this.responseText);
+      var obj = JSON.parse(this.responseText);
+      console.log("SID="+obj.sid);
+      addURL(obj.sid,url);
+  }
+});
+
+console.log("Lancement QNAP get SID");
+xhr.open("POST", "http://"+NASaddr+":"+NASport+"/downloadstation/V4/Misc/Login");
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=utf-8");
+xhr.setRequestHeader("User-Agent", "Qget 1.4.0 rv:80 (iPhone; iOS 13.3.1; fr_FR)");
+
+xhr.send(data);
+}
+
+/* +++++++++++++++++++++++++++++++++
+ Add download task using SID
+*/
+function addURL(sid, url) {
+    //var data = "sid="+sid+"&temp=Download&move=Multimedia%2FTemp&url=http%3A%2F%2Freleases.ubuntu.com%2F18.04%2Fubuntu-18.04.4-desktop-amd64.iso";
+    console.log("SID="+sid);
+    console.log("URL="+url);
+
+    var urlQNAP = url.replace(/\//g,"%2F");
+    urlQNAP = urlQNAP.replace(/:/,"%3A");
+    console.log("urlQNAP="+urlQNAP);
+
+    var dirQNAP = NASdir.replace(/\//g,"%2F");
+    dirQNAP = dirQNAP.replace(/:/,"%3A");
+    console.log("dirQNAP="+dirQNAP);
+
+    var data = "sid="+sid+"&temp=Download&move="+dirQNAP+"&url="+urlQNAP;
+    //var data = "sid="+sid+"&temp=Download&move=Multimedia%2FTemp&url="+urlQNAP;
+
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener("readystatechange", function() {
+        if(this.readyState === 4) {
+            console.log(this.responseText);
+        }
+    });
+
+    xhr.open("POST", "http://192.168.0.35:8080/downloadstation/V4/Task/AddUrl");
+    xhr.setRequestHeader("User-Agent", "Qget 1.4.0 rv:80 (iPhone; iOS 13.3.1; fr_FR)");
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=utf-8");
+    console.log(xhr);
+    xhr.send(data);
+
+    }
---
+++ popup/Configure_QNAP_Access.js
@@ -0,0 +1,65 @@
+function saveOptions(e) {
+  chrome.storage.local.set({
+    NASaddress: document.querySelector("#NASaddress").value
+  });
+  chrome.storage.local.set({
+    NASport: document.querySelector("#NASport").value
+  });
+  chrome.storage.local.set({
+    NASlogin: document.querySelector("#NASlogin").value
+  });
+  chrome.storage.local.set({
+    NASpassword: document.querySelector("#NASpassword").value
+  });
+  chrome.storage.local.set({
+    NASdir: document.querySelector("#NASdir").value
+  });
+//  e.preventDefault();
+}
+
+function restoreOptions() {
+  function onError(error) {
+    console.log(`Error: ${error}`);
+  }
+    
+ function setCurrentAddress(res){
+     document.querySelector("#NASaddress").value = res.NASaddress || "192.168.0.2" ;
+     console.log("Set DOM "+document.querySelector("#NASaddress").value+" with "+res.NASaddress || "192.168.0.2")
+ }   
+    
+    chrome.storage.local.get('NASaddress',setCurrentAddress);
+
+function setCurrentPort(res){
+     document.querySelector("#NASport").value = res.NASport || "80" ;
+     console.log("Set DOM "+document.querySelector("#NASport").value+" with "+res.NASport || "80")
+ }   
+    
+    chrome.storage.local.get('NASport',setCurrentPort);
+
+ function setCurrentLogin(res){
+     document.querySelector("#NASlogin").value = res.NASlogin || "admin" ;
+      console.log("Set DOM "+document.querySelector("#NASlogin").value+" with "+res.NASlogin || "admin" )
+}   
+    
+    chrome.storage.local.get('NASlogin',setCurrentLogin);
+
+function setCurrentPassword(res){
+     document.querySelector("#NASpassword").value = res.NASpassword || "aabbccdd" ;
+      console.log("Set DOM "+document.querySelector("#NASpassword").value+" with "+res.NASpassword || "aabbccdd")
+}   
+    
+    chrome.storage.local.get('NASpassword',setCurrentPassword);
+    
+    
+    
+function setCurrentDir(res){
+     document.querySelector("#NASdir").value = res.NASdir || "Multimedia/Films" ;
+      console.log("Set DOM "+document.querySelector("#NASdir").value+" with "+res.NASdir || "Multimedia/Films" )
+}   
+    
+    chrome.storage.local.get('NASdir',setCurrentDir);
+}
+
+document.addEventListener('DOMContentLoaded', restoreOptions);
+chrome.storage.onChanged.addListener(restoreOptions);
+document.querySelector("form").addEventListener("submit", saveOptions);
